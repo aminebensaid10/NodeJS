4e965bba0b9e2c25e80592e82715c84a9e4cd947
aminebensaid10 <92233451+aminebensaid10@users.noreply.github.com>
2023-02-14 16:28:48 +0100

    first commit

---
+++ atelier/models/contact.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose') ;
+const schema = mongoose.Schema;
+
+var Contact = new mongoose.Schema({
+fullName : String ,
+phone : Number 
+}) 
+module.exports= mongoose.model('contact',Contact) ;
