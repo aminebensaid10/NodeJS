3d6ad6916d39cc8bedfa899833195a24671334ed
aminebensaid10 <92233451+aminebensaid10@users.noreply.github.com>
2023-02-14 20:04:08 +0100

    crud

--- atelier/routes/contacts.js
+++ atelier/routes/contacts.js
@@ -5,6 +5,12 @@ router.get('/',(req,res,next) =>{
 res.json({message: 'hello'}) ;
 }); 
 
+router.get('/:id',(req,res,next)=> {
+    Contact.findById(req.params.id,(err, cont)=>{
+            res.json(cont) ;
+    })
+});
+
 router.post('/', (req,res,next) =>{
     var contact= new Contact({fullName:req.body.contactName , phone:req.body.contactNumber})
     contact.save((err,newContact)=>{
@@ -15,4 +21,19 @@ if (err){
 }
     })
     });  
+    router.put('/:id' , (req , res , next)=> {
+  
+        Contact.updateOne({ id: req.params.id , fullName : req.body.contactName , phone :req.body.contactTel },(err,data)=>{
+          if(err) return done(err);
+          res.json(data)
+        });
+      });
+
+      router.delete('/:id' , (req , res , next)=> {
+  
+        Contact.deleteOne({ id: req.params.id },(err,data)=>{
+          if(err) return done(err);
+          res.json(data)
+        });
+      });
 module.exports = router ;
